a8244b4c8fa158e6f0005c74c460893021ee4b55
KietDang1608 <kietdang1608@gmail.com>
2022-12-06 22:27:19 +0700

    1st update

---
+++ assets/js/aboutme.js
@@ -0,0 +1,39 @@
+var op1 = document.getElementById("am-ll-inf");
+var op2 = document.getElementById("am-st-inf");
+var op3 = document.getElementById("am-ht-inf");
+var imgshow= document.getElementById('am-r-img')
+
+var choose = document.getElementsByClassName('am-option')
+choose[0].style.textDecoration = 'underline';
+function switchToLL(){
+    op1.style.display = 'block';
+    op2.style.display = 'none';
+    op3.style.display = 'none';
+    for (var i = 0;i<choose.length;i++){
+        choose[i].style.textDecoration = 'none'
+    }
+    choose[0].style.textDecoration = 'underline';
+    imgshow.innerHTML = `<img src="/Mysite/assets/images/self_img2.jpg" alt="">`
+
+}
+function switchToST(){
+    op1.style.display = 'none';
+    op2.style.display = 'block';
+    op3.style.display = 'none';
+    for (var i = 0;i<choose.length;i++){
+        choose[i].style.textDecoration = 'none'
+    }
+    choose[1].style.textDecoration = 'underline';
+    imgshow.innerHTML = `<img src="/Mysite/assets/images/self_img3.jpg" alt="">`
+}
+function switchToHT(){
+    op1.style.display = 'none';
+    op2.style.display = 'none';
+    op3.style.display = 'block';
+    for (var i = 0;i<choose.length;i++){
+        choose[i].style.textDecoration = 'none'
+    }
+    choose[2].style.textDecoration = 'underline';
+    imgshow.innerHTML = `<img src="/Mysite/assets/images/self_img4.jpg" alt="">`
+}
+
---
+++ assets/js/images.js
@@ -0,0 +1,80 @@
+let list_hn_img = [
+    '/Mysite/assets/images/gallery/hn_01.jpg',
+    '/Mysite/assets/images/gallery/hn_02.jpg',
+    '/Mysite/assets/images/gallery/hn_03.jpg',
+    '/Mysite/assets/images/gallery/hn_04.jpg',
+    '/Mysite/assets/images/gallery/hn_05.jpg',
+    '/Mysite/assets/images/gallery/hn_06.jpg',
+]
+let list_dc_img = [
+    '/Mysite/assets/images/gallery/dc_01.jpg',
+    '/Mysite/assets/images/gallery/dc_02.jpg',
+    '/Mysite/assets/images/gallery/dc_03.jpg',
+    '/Mysite/assets/images/gallery/dc_04.jpg',
+    '/Mysite/assets/images/gallery/dc_05.jpg',
+    '/Mysite/assets/images/gallery/dc_06.jpg',
+    '/Mysite/assets/images/gallery/dc_07.jpg',
+    '/Mysite/assets/images/gallery/dc_08.jpg',
+    '/Mysite/assets/images/gallery/dc_09.jpg',
+    '/Mysite/assets/images/gallery/dc_10.jpg',
+    '/Mysite/assets/images/gallery/dc_11.jpg',
+]
+let list_c3_img = [
+    '/Mysite/assets/images/gallery/c3_01.jpg',
+    '/Mysite/assets/images/gallery/c3_02.jpg',
+    '/Mysite/assets/images/gallery/c3_03.jpg',
+    '/Mysite/assets/images/gallery/c3_04.jpg',
+    '/Mysite/assets/images/gallery/c3_05.jpg',
+    '/Mysite/assets/images/gallery/c3_06.jpg',
+    '/Mysite/assets/images/gallery/c3_07.jpg',
+    '/Mysite/assets/images/gallery/c3_08.jpg',
+    '/Mysite/assets/images/gallery/c3_09.jpg',
+]
+let list_ky_img = [
+    '/Mysite/assets/images/gallery/ky_01.jpg',
+    '/Mysite/assets/images/gallery/ky_02.jpg',
+    '/Mysite/assets/images/gallery/ky_03.jpg',
+    '/Mysite/assets/images/gallery/ky_04.jpg',
+    '/Mysite/assets/images/gallery/ky_05.jpg',
+    '/Mysite/assets/images/gallery/ky_06.jpg',
+]
+var choose = document.getElementsByClassName('i-option')
+choose[0].style.background = '#FFFBC1';
+choose[0].style.color = '#A9AF7E';
+function showLst(list){
+    let pos = document.getElementById('i-layout')
+    pos.innerHTML = ''
+    for (let i = 0;i<list.length;i++){
+        pos.innerHTML +=`<img src="${list[i]}" alt="">`
+    }
+}
+showLst(list_hn_img)
+function showImgs(type){
+    let num=0;
+    switch(type){
+        case 'hn':
+            showLst(list_hn_img);
+            num=0;
+            break;
+        case 'c3':
+            showLst(list_c3_img);
+            num=1;
+            break;
+        case 'dc':
+            showLst(list_dc_img);
+            num=3;
+            break;
+        case 'ky':
+            showLst(list_ky_img);
+            num=2;
+            break;
+    }
+    for(let i =0;i<choose.length;i++){
+        choose[i].style.background = '#A9AF7E';
+        choose[i].style.color = '#FFFBC1';
+    }
+    choose[num].style.background = '#FFFBC1';
+    choose[num].style.color = '#A9AF7E';
+}
+
+
